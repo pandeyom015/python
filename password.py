d14eb298eb6bd3e4887bce71a695b3b7a1cda48d
pandeyom015 <93940285+pandeyom015@users.noreply.github.com>
2026-02-14 11:08:49 +0530

    done

--- password.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-a="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXZ0123456789!@#$^&*()?"
-length=int(input("The length of the password is= "))
-password=""
-for  i in range(length):
-    password+=random.choice(a)
-print(password)
--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-a=int(input("Enter your age: "))
-print("Your age is",a)
-if(a>18):
-    print("You can drive the car")
-else:
-    print("You cannot drive the car")
